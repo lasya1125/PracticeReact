985c740869eb276f2d8ca23b0f27ff63f18614bf
Lasya Pullakhandam <lasya@live.unc.edu>
2021-02-25 16:58:00 -0500

    most recent deploy

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   // tabletop uses the google sheets key to pull the data
   useEffect(() => {
     Tabletop.init({
-      key: "1Si4u7vG-dbrfryb_HBKsWSYEzytpv7dcbWDWKbpU5Lk",
+      key: "1Si4u7vG-dbrfryb_HBKsWSYEzytpv7dcbWDWKbpU5Lk", //1Si4u7vG-dbrfryb_HBKsWSYEzytpv7dcbWDWKbpU5Lk
       simpleSheet: true
     })
       .then((data) => setData(data))
@@ -34,58 +34,65 @@ function App() {
 
   //stores the search query
   const query = new URLSearchParams(search).get('s');
+  console.log(query);
 
   const [searchQuery, setSearchQuery] = useState(query || '');
-    //creates an array of only relevant rows
+  //creates an array of only relevant rows
   const filteredPosts = filterPosts(data, searchQuery);
   // after getting data this displays each item 
-  return (  
+  return (
     <>
-    <Router>
-    <div>
-      {/* Allows screenreaders to know that the table has changed */}
-    <Announcer message={`Table has ${filteredPosts.length} rows`}/>
-    <Container>
-  <Row className="justify-content-md-center">
-      <h1>UNC App Lab Skills Tree</h1>
-    </Row>
-  <Row className="justify-content-md-center">
-      <Search 
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
-  </Row>
-       
-      </Container>
-      <Table striped bordered>
-        <thead> 
-          <th> Name </th>
-          <th> Email </th>
-          <th> OS </th>
-          <th> Coding Language </th>
-          <th> IDEs </th>
-          <th> Frameworks </th>
-          <th> Application </th>
-          <th> Additional Info </th>
-        </thead>
-        <tbody>
-        {filteredPosts.map((item, i) => (
-          <Fragment key={i}>
-            <tr>
-              <td>{item.Name}</td>
-              <td>{item.Email}</td>
-              <td>{item.Concat_Operating_System}</td>
-              <td>{item.Concat_Coding_Language}</td>
-              <td>{item.Concat_IDE}</td>
-              <td>{item.Concat_Framework}</td>
-              <td>{item.Concat_Application}</td>
-              <td><a  href={item.Website}>{item.Website}</a></td>
-            </tr>
-          </Fragment>
-        ))}
-        </tbody>
-      </Table>
-      </div>
+      <Router>
+        <div>
+          {/* Allows screenreaders to know that the table has changed */}
+          <Announcer message={`Table has ${filteredPosts.length} rows`} />
+          <Container>
+            <Row className="justify-content-md-center">
+              <h1>UNC App Lab Skills Tree</h1>
+            </Row>
+
+            <Row className="justify-content-md-center">
+              <p>The Skills Tree is a resource to contact peers regarding questions or interests around a specific technology or area. If you would like to be included in this list and are open to being contacted about the skills you list, please fill out this form <a href="https://forms.gle/Tujwhgu6GEsY52zC6">here</a>.</p>
+            </Row>
+
+            <Row className="justify-content-md-center">
+              <Search
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+              />
+            </Row>
+
+          </Container>
+          <Table striped bordered>
+            <thead>
+              <th> Name </th>
+              <th> Email </th>
+              <th> OS </th>
+              <th> Coding Language </th>
+              <th> IDEs </th>
+              <th> Frameworks </th>
+              <th> Application </th>
+              <th> Additional Info </th>
+            </thead>
+            <tbody>
+              {filteredPosts.map((item, i) => (
+                <Fragment key={i}>
+                  <tr>
+                    <td>{item.Name}</td>
+                    <td>{item.Email}</td>
+                    <td>{item.Concat_Operating_System}</td>
+                    <td>{item.Concat_Coding_Language}</td>
+                    <td>{item.Concat_IDE}</td>
+                    <td>{item.Concat_Framework}</td>
+                    <td>{item.Concat_Application}</td>
+                    <td>{item.Website}</td> 
+                    {/* Max has two websites entered so how do we get href to put links separately */}
+                  </tr>
+                </Fragment>
+              ))}
+            </tbody>
+          </Table>
+        </div>
       </Router>
     </>
   );
@@ -94,32 +101,32 @@ function App() {
 
 const filterPosts = (posts, query) => {
   if (!query) {
-      return posts;
+    return posts;
   }
 
   return posts.filter((post) => {
-      const searchQuery = query.toLowerCase();
-      let concatArray = new Array();
+    const searchQuery = query.toLowerCase();
+    let concatArray = new Array();
 
-      const codingLanguage = post.Concat_Coding_Language.toLowerCase();
-      const application = post.Concat_Application.toLowerCase();
-      const framework = post.Concat_Framework.toLowerCase();
-      const ide = post.Concat_IDE.toLowerCase();
-      const operatingsystem = post.Concat_Operating_System.toLowerCase();
+    const codingLanguage = post.Concat_Coding_Language.toLowerCase();
+    const application = post.Concat_Application.toLowerCase();
+    const framework = post.Concat_Framework.toLowerCase();
+    const ide = post.Concat_IDE.toLowerCase();
+    const operatingsystem = post.Concat_Operating_System.toLowerCase();
 
-      concatArray.push(codingLanguage);
-      concatArray.push(application);
-      concatArray.push(framework);
-      concatArray.push(ide);
-      concatArray.push(operatingsystem);
+    concatArray.push(codingLanguage);
+    concatArray.push(application);
+    concatArray.push(framework);
+    concatArray.push(ide);
+    concatArray.push(operatingsystem);
 
-      //Checks to see if the term is present in the row
-      for (let i = 0 ; i < concatArray.length ; i++){
-        if (concatArray[i].includes(searchQuery)) {
-          return true;
-        }
+    //Checks to see if the term is present in the row
+    for (let i = 0; i < concatArray.length; i++) {
+      if (concatArray[i].includes(searchQuery)) {
+        return true;
       }
-      return false;
+    }
+    return false;
   });
 };
 
